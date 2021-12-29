200c66e946274e1e982895f20313f6772fdcc208
Philipp Krink <krink.philipp@gmail.com>
2021-12-29 17:38:07 +0100

    Fetching from URL and parsing to JSON works

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,5 +1,11 @@
+require 'json'
+require 'open-uri'
+
 class Api::V1::LocationsController < Api::V1::BaseController
   def index
+    @url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=13.437641,52.494857&access_token=pk.eyJ1IjoiZmx1ZW5jYSIsImEiOiJja3g0dDJodnIxOWpmMnBwaDluZGFqdDVuIn0.cv0P-t75vYUqbT5hfq7k6g"
+    response_string = URI.open(@url).read
+    @response = JSON.parse(response_string)
     place = {
       lat: params[:lat],
       long: params[:lng]
