260807911c1dd476fa9d119d24e6948517f737c1
Philipp Krink <krink.philipp@gmail.com>
2021-12-29 17:54:34 +0100

    testing

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::LocationsController < Api::V1::BaseController
     @url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=13.437641,52.494857&access_token=pk.eyJ1IjoiZmx1ZW5jYSIsImEiOiJja3g0dDJodnIxOWpmMnBwaDluZGFqdDVuIn0.cv0P-t75vYUqbT5hfq7k6g"
     response_string = URI.open(@url).read
     @response = JSON.parse(response_string)
+    raise
     place = {
       lat: params[:lat],
       long: params[:lng]
