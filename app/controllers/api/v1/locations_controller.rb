f085590c3a756293cad4fde968e21dc3014b8c74
Philipp Krink <krink.philipp@gmail.com>
2022-01-03 15:47:22 +0100

    Added dotenv Gem

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 class Api::V1::LocationsController < Api::V1::BaseController
   def index
-    @url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=13.437641,52.494857&access_token=pk.eyJ1IjoiZmx1ZW5jYSIsImEiOiJja3g0dDJodnIxOWpmMnBwaDluZGFqdDVuIn0.cv0P-t75vYUqbT5hfq7k6g"
+    @url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museum.json?type=poi&proximity=#{params[:lng]},#{params[:lat]}&access_token=#{ENV['MAPBOX_API_KEY']}"
     response_string = URI.open(@url).read
     @response = JSON.parse(response_string)
     raise
